49bacf6550d3f1e162ec87d3c13d66018e6cdd9f
felix <6244794@cegepoutaouais.qc.ca>
2026-01-29 14:01:53 -0500

    ajout favicon et controller

---
+++ S2-5 LePetitProjet/Controllers/RecetteController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace S2_5_LePetitProjet.Controllers
+{
+    public class RecetteController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["title"] = "recetteeee";
+            return View("index");
+        }
+    }
+}
