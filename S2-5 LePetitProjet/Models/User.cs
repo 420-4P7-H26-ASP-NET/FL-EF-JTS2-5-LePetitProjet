5746246cc7f5f032dd251f1f8188515ee534b2d1
Julien-Turcotte <6315237@cegepoutaouais.qc.ca>
2026-01-29 14:49:31 -0500

    basic user model

---
+++ S2-5 LePetitProjet/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace S2_5_LePetitProjet.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; } //regex
+        public string Password { get; set; } //regex
+        public string Role { get; set; }
+    }
+}
