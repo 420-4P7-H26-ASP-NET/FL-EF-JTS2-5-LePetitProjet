540bc77b0b6494a2416be0f6ab77f64ad6d093f0
Felix <6244794@cegepoutaouais.qc.ca>
2026-01-29 12:27:26 -0500

    Ajoutez des fichiers projet.

---
+++ S2-5 LePetitProjet/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using S2_5_LePetitProjet.Models;
+
+namespace S2_5_LePetitProjet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ S2-5 LePetitProjet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace S2_5_LePetitProjet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
