976195431bfb9153e8d56bd341161a917c6ba76f
felix <6244794@cegepoutaouais.qc.ca>
2026-01-29 17:04:03 -0500

    add email regex

--- S2-5 LePetitProjet/Models/User.cs
+++ S2-5 LePetitProjet/Models/User.cs
@@ -1,10 +1,27 @@
-﻿namespace S2_5_LePetitProjet.Models
+﻿using System.Text.RegularExpressions;
+
+namespace S2_5_LePetitProjet.Models
 {
     public class User
     {
+        private string _email;
+        private string _password;
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; } //regex
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                string pattern = @".+@.+\..+";
+                Regex regex = new Regex(pattern);
+                if (regex.IsMatch(value))
+                {
+                    _email = value;
+                }
+            }
+        }
         public string Password { get; set; } //regex
         public string Role { get; set; }
     }
